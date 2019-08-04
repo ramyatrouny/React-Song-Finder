f1781c3b0bf78e4fedab6901671a4e773cbc525c
mba3gar <ramiatrouni@gmail.com>
2019-08-04 19:20:06 +0300

    Fixing the env variable

--- src/components/tracks/Lyrics.js
+++ src/components/tracks/Lyrics.js
@@ -14,11 +14,11 @@ class Lyrics extends Component {
 
 
     componentDidMount() {
-        axios.get(`https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=${this.props.match.params.id}&apikey=${process.env.MUSIXMATCHKEY}`)
+        axios.get(`https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=${this.props.match.params.id}&apikey=${process.env.REACT_APP_MUSIXMATCHKEY}`)
             .then(res => {
                 this.setState({ lyrics: res.data.message.body.lyrics });
 
-                return axios.get(`https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.get?track_id=${this.props.match.params.id}&apikey=${process.env.MUSIXMATCHKEY}`)
+                return axios.get(`https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.get?track_id=${this.props.match.params.id}&apikey=${process.env.REACT_APP_MUSIXMATCHKEY}`)
                     .then(res => {
                         this.setState({ track: res.data.message.body.track })
                     }).catch(err => {
